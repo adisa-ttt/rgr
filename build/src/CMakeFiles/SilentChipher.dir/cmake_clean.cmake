910c16c341c70d17c1363dce91246144036815ac
adisa <adiss@adis.IGD_Rostelecom>
2026-06-10 16:00:55 +0700

    Добавлен корневой CMake файл, подключающий все папки, и который собирает проект SilentChipher

---
+++ build/src/CMakeFiles/SilentChipher.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SilentChipher.dir/main.cpp.o"
+  "CMakeFiles/SilentChipher.dir/main.cpp.o.d"
+  "CMakeFiles/SilentChipher.dir/plugin_loader.cpp.o"
+  "CMakeFiles/SilentChipher.dir/plugin_loader.cpp.o.d"
+  "SilentChipher"
+  "SilentChipher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SilentChipher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
